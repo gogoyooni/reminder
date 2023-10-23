fae651217941cebb588f6ffe3c30b402ff9b0fe6
gogoyooni <taeyoondev@gmail.com>
2023-10-23 23:57:55 +0900

    changed div to a TaskCard component

--- components/CollectionCard.tsx
+++ components/CollectionCard.tsx
@@ -30,6 +30,7 @@ import { deleteCollection } from "@/actions/collection";
 import { toast } from "./ui/use-toast";
 import { useRouter } from "next/navigation";
 import CreateTaskDialog from "./CreateTaskDialog";
+import TaskCard from "./TaskCard";
 
 interface Props {
   collection: Collection & {
@@ -89,13 +90,29 @@ const CollectionCard = ({ collection }: Props) => {
           </Button>
         </CollapsibleTrigger>
         <CollapsibleContent className="flex rounded-b-md flex-col dark:bg-neutral-900 shadow-lg">
-          {tasks.length === 0 && <div>No tasks</div>}
+          {tasks.length === 0 && (
+            <Button
+              variant={"ghost"}
+              className="flex items-center justify-center gap-1 p-8 py-12 rounded-none"
+              onClick={() => setShowCreateModal(true)}
+            >
+              <p>No tasks yet : </p>
+              <span
+                className={cn(
+                  "text-sm bg-clip-text text-transparent",
+                  CollectionColors[collection.color as CollectionColor]
+                )}
+              >
+                Create one
+              </span>
+            </Button>
+          )}
           {
             <>
               <Progress className="rounded-none" value={55} />
               <div className="p-4 gap-3 flex flex-col">
                 {tasks.map((task: any) => (
-                  <div key={task.id}>{task.content}</div>
+                  <TaskCard key={task.id} task={task} />
                 ))}
               </div>
             </>
---
+++ components/TaskCard.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Task } from "@prisma/client";
+import { Checkbox } from "./ui/checkbox";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
+
+function getExpirationColor(expiresAt: Date) {
+  const days = Math.floor(expiresAt.getTime() - Date.now()) / 1000 / 60 / 60;
+
+  if (days < 0) return "text-gray-300 dark:text-gray-300";
+
+  if (days <= 3 * 24) return "text-red-500 dark:text-red-400";
+  if (days <= 7 * 24) return "text-orange-500 dark:text-red-400";
+}
+
+const TaskCard = ({ task }: { task: Task }) => {
+  return (
+    <div className="flex gap-2 items-start">
+      <Checkbox id={task.id.toString()} className="w-5 h-5" checked={task.done} />
+      <label
+        className={cn(
+          "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 decoration-1 dark:decoration-white",
+          task.done && "line-through"
+        )}
+      >
+        {task.content}
+        {task.expiresAt && (
+          <p
+            className={cn(
+              "text-xs text-neutral-500 dark:text-neutral-400",
+              getExpirationColor(task.expiresAt)
+            )}
+          >
+            {format(task.expiresAt, "MM/dd/yyyy")}
+          </p>
+        )}
+      </label>
+    </div>
+  );
+};
+
+export default TaskCard;
